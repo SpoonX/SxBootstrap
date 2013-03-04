c8039f928ac77e24e7b4a71dca97d116ff42392c
Ronald Baltus <ronaldbaltus@gmail.com>
2013-03-04 10:46:36 +0100

    Refactored Label view helper

--- src/SxBootstrap/View/Helper/Bootstrap/Label.php
+++ src/SxBootstrap/View/Helper/Bootstrap/Label.php
@@ -8,8 +8,8 @@
  */
 namespace SxBootstrap\View\Helper\Bootstrap;
 
-use Zend\Form\View\Helper\AbstractHelper;
 use SxBootstrap\Exception;
+use SxBootstrap\Html\HtmlElement;
 
 /**
  * Label
@@ -18,119 +18,86 @@ use SxBootstrap\Exception;
  * @package    SxBootstrap_View
  * @subpackage Helper
  */
-class Label extends AbstractHelper
+class Label extends AbstractElementHelper
 {
-
-    /**
-     * @var string
-     */
-     protected $format = '<span class="label%s">%s</span>';
-
     /**
      * Display an Informational Label
      *
-     * @param   string  $label
-     *
-     * @return  string
+     * @return \SxBootstrap\View\Helper\Bootstrap\Label
      */
-    public function info($label)
+    public function info()
     {
-        return $this->render($label, 'label-info');
+        return $this->addClass('label-info');
     }
 
     /**
      * Display an Important Label
      *
-     * @param   string  $label
-     *
-     * @return  string
+     * @return \SxBootstrap\View\Helper\Bootstrap\Label
      */
-    public function important($label)
+    public function important()
     {
-        return $this->render($label, 'label-important');
+        return $this->addClass('label-important');
     }
 
     /**
      * Display an Inverse Label
      *
-     * @param   string  $label
-     *
-     * @return  string
+     * @return \SxBootstrap\View\Helper\Bootstrap\Label
      */
-    public function inverse($label)
+    public function inverse()
     {
-        return $this->render($label, 'label-inverse');
+        return $this->addClass('label-inverse');
     }
 
     /**
      * Display a Sucess Label
      *
-     * @param   string  $label
-     *
-     * @return  string
+     * @return \SxBootstrap\View\Helper\Bootstrap\Label
      */
-    public function success($label)
+    public function success()
     {
-        return $this->render($label, 'label-success');
+        return $this->addClass('label-success');
     }
 
     /**
      * Display a Warning Label
      *
-     * @param   string  $label
-     *
-     * @return  string
+     * @return \SxBootstrap\View\Helper\Bootstrap\Label
      */
-    public function warning($label)
+    public function warning()
     {
-        return $this->render($label, 'label-warning');
+        return $this->addClass('label-warning');
     }
 
     /**
      * Render an Label
      *
-     * @param   string  $label
-     * @param   string  $class
-     *
      * @return  string
      * @throws  Exception\InvalidArgumentException
      */
-    public function render($label, $class = '')
+    public function render()
     {
-        if (!is_string($label)) {
-            throw new Exception\InvalidArgumentException(sprintf(
-                'Label must be of type string, got "%s".',
-                gettype($label)
-            ));
-        }
-
-        if (!is_string($class)) {
-            throw new Exception\InvalidArgumentException(sprintf(
-                'Provided class must be of type string, got "%s".',
-                gettype($class)
-            ));
-        }
-        
-        if (!empty($class)) {
-            $class = ' ' . trim($class, ' ');
-        }
-
-        return sprintf($this->format, $class, $label);
+        return $this->getElement()->render();
     }
 
     /**
      * Invoke Label
      *
      * @param   string  $label
-     * @param   string  $class
      *
-     * @return  string|Label
+     * @return  \SxBootstrap\View\Helper\Bootstrap\Label
      */
-    public function __invoke($label = null, $class = '')
+    public function __invoke($label = null)
     {
-        if ($label) {
-            return $this->render($label, $class);
+        $this->setElement(new HtmlElement('span'));
+
+        if (!is_null($label)) {
+            $this->setContent($label);
         }
-        return $this;
+
+        $this->addClass('label');
+
+        return clone $this;
     }
 }
